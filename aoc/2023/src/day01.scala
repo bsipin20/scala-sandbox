ad67eb18087e3973f66d0f3fe44c2ac839f33dcb
brian <bsipin@gmail.com>
2024-03-07 20:57:57 -0500

    removing extra

--- aoc/2023/src/day01.scala
+++ aoc/2023/src/day01.scala
@@ -39,31 +39,12 @@ val stringDigitReprs = Map(
   "nine" -> 9,
 )
 
-/** All the string representation of digits, including the digits themselves. */
 val digitReprs = stringDigitReprs ++ (1 to 9).map(i => i.toString() -> i)
 
 def part2(input: String): String =
-  // A regex that matches any of the keys of `digitReprs`
   val digitReprRegex = digitReprs.keysIterator.mkString("|").r
 
   def lineToCoordinates(line: String): Int =
-    /* Find all the digit representations in the line.
-     *
-     * A first attempt was to use
-     *
-     *   val matches = digitReprRegex.findAllIn(line).toList
-     *
-     * however, that misses overlapping string representations of digits.
-     * In particular, it will only find "one" in "oneight", although it should
-     * also find the "eight".
-     *
-     * In our dataset, we had the line "29oneightt" which must parse as 28, not 21.
-     *
-     * Therefore, we explicitly try a find a match starting at each position
-     * of the line. This is equivalent to finding a *prefix* match at every
-     * *suffix* of the line. `line.tails` conveniently iterates over all the
-     * suffixes.
-     */
     val matchesIter =
       for
         lineTail <- line.tails
@@ -72,13 +53,11 @@ def part2(input: String): String =
         oneMatch
     val matches = matchesIter.toList
 
-    // Convert the string representations into actual digits and form the result
     val firstDigit = digitReprs(matches.head)
     val lastDigit = digitReprs(matches.last)
     s"$firstDigit$lastDigit".toInt
   end lineToCoordinates
 
-  // Process lines as in part1
   val result = input
     .linesIterator
     .map(lineToCoordinates(_))
